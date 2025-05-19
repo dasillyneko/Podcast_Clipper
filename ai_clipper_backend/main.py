cb58892a2cccbd7ee0cf72b6b7af0d489db96a89
dasillyneko <jenaaryaman@gmail.com>
2025-05-19 06:45:19 +0530

    initial commit

---
+++ ai_clipper_backend/main.py
@@ -0,0 +1,57 @@
+import glob
+import json
+import pathlib
+import pickle
+import shutil
+import subprocess
+import time
+import uuid
+import boto3
+import cv2
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+import ffmpegcv
+import modal
+import numpy as np
+from pydantic import BaseModel
+import os
+from google import genai
+
+import pysubs2
+from tqdm import tqdm
+import whisperx
+
+
+class ProcessVideoRequest(BaseModel):
+    s3_key: str
+
+image = (modal.Image.from_registry(
+    "nvidia/cuda:12.4.0-devel-ubuntu22.04", add_python="3.12")
+    .apt_install(["ffmpeg", "libgl1-mesa-glx", "wget", "libcudnn8", "libcudnn8-dev"])
+    .pip_install_from_requirements("requirements.txt")
+    .run_commands(["mkdir -p /usr/share/fonts/truetype/custom",
+                   "wget -O /usr/share/fonts/truetype/custom/Anton-Regular.ttf https://github.com/google/fonts/raw/main/ofl/anton/Anton-Regular.ttf",
+                   "fc-cache -f -v"])
+    .add_local_dir("LR-ASD", "/LR-ASD", copy=True))
+
+app = modal.App("ai-podcast-clipper", image=image)
+
+volume = modal.Volume.from_name(
+    "ai-podcast-clipper-model-cache", create_if_missing=True
+)
+
+mount_path = "/root/.cache/torch"
+
+auth_scheme = HTTPBearer()
+
+@app.cls(gpu="L40S", timeout=900, retries=0, scaledown_window=20, secrets=[modal.Secret.from_name("ai-podcast-clipper-secret")], volumes={mount_path: volume})
+class AiPodcastClipper:
+    @modal.enter()
+    def load_model(self):
+        print("Loading model...")
+        pass
+  
+    @modal.fastapi_endpoint(method="POST")
+    def process_video(self,request: ProcessVideoRequest,token: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+        print("Processing video...")
+        pass
